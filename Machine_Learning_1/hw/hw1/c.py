37a8d79bde4978fbdae5f1fec7fb3919b3e7fa34
grach0v <justwantpost@gmail.com>
2022-10-03 02:30:00 +0300

    deadlines are here

--- Machine_Learning_1/hw/hw1/c.py
+++ Machine_Learning_1/hw/hw1/c.py
@@ -2,12 +2,16 @@ change = float(input())
 coins = [0.01, 0.05, 0.1, 0.5, 1, 2, 5, 10]
 i = len(coins) - 1
 
+ans = ''
+
 while abs(change) >= 1e-6 and i >= 0:
     change = round(change * 100) / 100
     n = int((change + 1e-6) / coins[i])
     change -= n * coins[i]
 
     if n != 0:
-        print(f'{coins[i]:5.2f}\t{n}')
+        ans += f'{coins[i]:5.2f}\t{n}\n'
 
     i -= 1
+
+print(ans)
