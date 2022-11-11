683c5a7546180c54af9ef084fb3f17950f9c1251
grach0v <justwantpost@gmail.com>
2022-11-11 09:21:30 +0300

    finished algorithms ml hw3

--- Machine_Learning_1/hw/hw3/knn/model_selection.py
+++ Machine_Learning_1/hw/hw3/knn/model_selection.py
@@ -6,6 +6,7 @@ from sklearn.model_selection import KFold, BaseCrossValidator
 from sklearn.metrics import accuracy_score
 
 from knn.classification import BatchedKNNClassifier
+from collections import defaultdict
 
 
 def knn_cross_val_score(X, y, k_list, scoring, cv=None, **kwargs):
@@ -21,28 +22,44 @@ def knn_cross_val_score(X, y, k_list, scoring, cv=None, **kwargs):
     elif not isinstance(cv, BaseCrossValidator):
         raise TypeError("cv should be BaseCrossValidator instance", type(cv))
 
+    ans = defaultdict(list)
 
-    ans = {}
-    for k in k_list:
-        ans[k] = []
+    for train_index, test_index in cv.split(X):
+        model = BatchedKNNClassifier(n_neighbors=max(k_list), **kwargs)
+        model.fit(X[train_index], y[train_index])
+        distances, indices = model.kneighbors(X[test_index], return_distance=True)
 
-        for train_index, test_index in cv.split(X):
-            model = BatchedKNNClassifier(n_neighbors=k)
-            model.fit(X[train_index], y[train_index])
-            y_pred = model.predict(X[test_index])
-
+        for k in k_list:
+            # Is this ok?
+            model._finder.n_neighbors = k
+            y_pred = model._predict_precomputed(indices[:, :k], distances[:, :k])
             ans[k].append(scorer(y[test_index], y_pred))
-        
+
+    for k in k_list:
         ans[k] = np.array(ans[k])
-    
+
     return ans
+
+    # for k in k_list:
+    #     ans[k] = []
+
+    #     for train_index, test_index in cv.split(X):
+    #         model = BatchedKNNClassifier(n_neighbors=k)
+    #         model.fit(X[train_index], y[train_index])
+    #         y_pred = model.predict(X[test_index])
+
+    #         ans[k].append(scorer(y[test_index], y_pred))
+        
+    #     ans[k] = np.array(ans[k])
+    
+    # return ans
 
     # Now this is a challenge
     # I will try to make it readable
     # return {
     #     k: np.array([
     #         scorer(
-    #             BatchedKNNClassifier(n_neighbors=k).fit(X[train_index], y[train_index]).predict(X[test_index]),
+    #             BatchedKNNClassifier(n_neighbors=k, **kwargs).fit(X[train_index], y[train_index]).predict(X[test_index]),
     #             y[test_index],
     #         )
     #         for train_index, test_index in cv.split(X)
